f7f8d193c2190c479c1a844a767b2cbcf16ff9d3
PBNDEVI <73999815+PBNDEVI@users.noreply.github.com>
2021-05-25 11:32:22 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
+
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-     <h1>hello world
-     </h1>
+    <div>
+      Hello {props.name}, you are {props.age} years old
+    </div>
+  )
+}
+function App() {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 }
-
 export default App;
